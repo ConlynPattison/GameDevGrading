3f1cf86c01b28a79c2fe95b8a623e85b4e26a48f
ConlynPattison <cpattison@csumb.edu>
2024-03-12 18:18:08 -0700

    Added page and routing for space invaders part 2

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import PongPart1 from "./routes/PongPart1";
 import PlatformerPart1 from "./routes/PlatformerPart1";
 import PlatformerPart2 from "./routes/PlatformerPart2";
 import SpaceInvadersPart1 from "./routes/SpaceInvadersPart1";
+import SpaceInvadersPart2 from "./routes/SpaceInvadersPart2";
 
 
 const router = createBrowserRouter([
@@ -31,6 +32,10 @@ const router = createBrowserRouter([
 	{
 		path: "space-invaders1",
 		element: <SpaceInvadersPart1 />
+	},
+	{
+		path: "space-invaders2",
+		element: <SpaceInvadersPart2 />
 	}
 ])
 
