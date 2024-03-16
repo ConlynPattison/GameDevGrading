a30b25ad4a9cbbe285ac00cda9ab96a61475b87c
ConlynPattison <cpattison@csumb.edu>
2024-03-16 11:21:03 -0700

    Fixed text for link to space invaders part 2

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -12,7 +12,7 @@ const Home = () => {
 			<p onClick={() => navigate("platformer1")}> Platformer Part 1 </p>
 			<p onClick={() => navigate("platformer2")}> Platformer Part 2 </p>
 			<p onClick={() => navigate("space-invaders1")}> Space Invaders Part 1 </p>
-			<p onClick={() => navigate("space-invaders2")}> Space Invaders Part 1 </p>
+			<p onClick={() => navigate("space-invaders2")}> Space Invaders Part 2 </p>
 		</div>
 	)
 }
